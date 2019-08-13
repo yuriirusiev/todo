ca143338a5c700df05b62a3e575ff5714c408f67
yuriirusiev <babakoshka@gmail.com>
2019-08-13 15:07:58 +0300

    finished todo app

--- src/components/main-form/index.js
+++ src/components/main-form/index.js
@@ -25,30 +25,33 @@ export default class MainForm extends React.Component {
 
   render() {
     return (
-      <form 
-        className="app__main-form"
-        onSubmit={this.onSubmit}
-      >
-        <div className="app__main-form-input__wrapper">
-          <input
-            className="app__main-form-input" 
-            type="text"
-            placeholder="reminder text"
-            value={this.state.inputText} 
-            onChange={this.onChange}
-          />
+      <div className="app__main-forn__wrapper">
+        <form 
+          className="app__main-form"
+          onSubmit={this.onSubmit}
+        >
+          <div className="app__main-form-input__wrapper">
+            <input
+              className="app__main-form-input" 
+              type="text"
+              placeholder="reminder text"
+              value={this.state.inputText} 
+              onChange={this.onChange}
+            />
+            <button 
+            className="app__main-form__button" 
+            type="submit" 
+            value="Submit" 
+            >
+              Add
+            </button>
+          </div>
           <p className="app__main-form__input-hint">
             at least 8 characters
           </p>
-        </div>
-        <button 
-          className="app__main-form__button" 
-          type="submit" 
-          value="Submit" 
-        >
-          Add
-        </button>
-      </form>
+
+        </form>
+      </div>
     )
   }
 }
